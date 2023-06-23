94b60728fc09f450585e68f4d6c306458e344c37
Nils Fischer <nils@designatic.de>
2023-06-23 21:09:29 +0200

    set StyledHeading to fixed-position + centered; add margin-top to PartsListContainer

--- components/PartsList/PartsList.styled.js
+++ components/PartsList/PartsList.styled.js
@@ -6,10 +6,16 @@ export const PartsListContainer = styled.section`
   border: 1px solid black;
   border-radius: 5px;
   margin: 0.5%;
+  margin-top: 2.5em;
 `;
 
 export const StyledHeading = styled.h1`
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  top: 0;
+  margin-block-start: 0;
+  margin-block-end: 0;
   color: blue;
-  align-self: center;
-  margin: 0.5%;
+  background-color: white;
 `;
