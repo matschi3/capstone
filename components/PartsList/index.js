56062b8cf6bed19143ea05d45df5949ae614fbdb
Nils Fischer <nils@designatic.de>
2023-06-23 16:45:41 +0200

    add styledHeading

--- components/PartsList/index.js
+++ components/PartsList/index.js
@@ -1,9 +1,10 @@
 import PartCard from "../PartCard";
-import { PartsListContainer } from "./PartsList.styled";
+import { PartsListContainer, StyledHeading } from "./PartsList.styled";
 
 export default function PartsList({ parts }) {
   return (
     <PartsListContainer>
+      <StyledHeading>PARTS</StyledHeading>
       {parts.map((part) => {
         return <PartCard key={part.id} part={part} />;
       })}
