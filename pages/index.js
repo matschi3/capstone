be729cbe2225774f584256aab8627511a7b45061
Nils Fischer <nils@designatic.de>
2023-06-23 13:27:08 +0200

    replaced PartCard with PartsList, add parts as prop; add import

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import PartCard from "@/components/PartCard";
 import { parts } from "@/lib/parts";
+import PartsList from "@/components/PartsList";
 
 export default function HomePage() {
-  return <PartCard />;
+  return <PartsList parts={parts} />;
 }
