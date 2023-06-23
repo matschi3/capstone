f8d27d15c3d27bf8193cfdb7cf7e2d922624bf4d
Nils Fischer <nils@designatic.de>
2023-06-23 16:25:34 +0200

    import and switched PartCardCategory as styled

--- components/PartCard/index.js
+++ components/PartCard/index.js
@@ -1,13 +1,16 @@
 import Image from "next/image";
-import { PartCardFlexContainer } from "./PartCard.styled";
+import { PartCardFlexContainer, PartCardCategory } from "./PartCard.styled";
 
 export default function PartCard({ part }) {
   return (
-    <PartCardFlexContainer direction={"row"}>
+    <PartCardFlexContainer direction={"column"}>
       <Image src={part.imgUrl} alt={part.name} width={100} height={100} />
       <p>Einkaufspreis: {part.purchasingPrice}</p>
       <p>Name: {part.name}</p>
-      <p>{part.category}</p>
+      <PartCardFlexContainer direction={"row"}>
+        <PartCardCategory>{part.category}</PartCardCategory>
+        <PartCardCategory>{part.category}</PartCardCategory>
+      </PartCardFlexContainer>
     </PartCardFlexContainer>
   );
 }
