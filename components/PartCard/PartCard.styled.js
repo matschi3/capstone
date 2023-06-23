8b698ebb8f8d5880def67be1ff397ea400cbf9f6
Nils Fischer <nils@designatic.de>
2023-06-23 16:45:12 +0200

    add prop style for border or not

--- components/PartCard/PartCard.styled.js
+++ components/PartCard/PartCard.styled.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const PartCardFlexContainer = styled.article`
   display: flex;
   flex-direction: ${(props) => props.direction || "column"};
-  border: 1px solid blue;
+  border: 1px solid ${(props) => props.border || "none"};
   border-radius: 5px;
   margin: 0.5%;
 `;
@@ -12,5 +12,6 @@ export const PartCardCategory = styled.p`
   border: 1px solid green;
   border-radius: 5px;
   background-color: green;
+  padding: 0.5%;
   margin: 0.5%;
 `;
