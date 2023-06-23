1076b800b8cc77aa34066182114160fdba067483
Nils Fischer <nils@designatic.de>
2023-06-23 13:38:08 +0200

    add part as prop; moved map.function to PartsList

--- components/PartCard/index.js
+++ components/PartCard/index.js
@@ -1,17 +1,14 @@
 import { parts } from "@/lib/parts";
 import Image from "next/image";
 
-export default function PartCard(params) {
+export default function PartCard({ part }) {
+  console.log(part);
   return (
-    <section>
-      {parts.map((part) => (
-        <article key={part._id}>
-          <Image src={part.imgUrl} alt={part.name} width={100} height={100} />
-          <p>Einkaufspreis: {part.purchasingPrice}</p>
-          <p>Name: {part.name}</p>
-          <p>{part.category}</p>
-        </article>
-      ))}
-    </section>
+    <article>
+      <Image src={part.imgUrl} alt={part.name} width={100} height={100} />
+      <p>Einkaufspreis: {part.purchasingPrice}</p>
+      <p>Name: {part.name}</p>
+      <p>{part.category}</p>
+    </article>
   );
 }
