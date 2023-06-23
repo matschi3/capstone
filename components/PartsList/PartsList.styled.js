c0bd1a31dd5a4c47eda3b9f61e1517a5d9f690cd
Nils Fischer <nils@designatic.de>
2023-06-23 16:02:16 +0200

    add basic border style

--- components/PartsList/PartsList.styled.js
+++ components/PartsList/PartsList.styled.js
@@ -3,4 +3,7 @@ import styled from "styled-components";
 export const PartsListContainer = styled.section`
   display: flex;
   flex-direction: column;
+  border: 1px solid black;
+  border-radius: 5px;
+  margin: 0.5%;
 `;
