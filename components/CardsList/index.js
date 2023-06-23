fd486db94f3acd40c8d2a431c3cecf5dfab04534
Nils Fischer <nils@designatic.de>
2023-06-23 12:29:52 +0200

    add return

--- components/CardsList/index.js
+++ components/CardsList/index.js
@@ -1,5 +1,11 @@
 import PartCard from "../PartCard";
 
 export default function PartsList(params) {
-  return <></>;
+  return (
+    <section>
+      {parts.map((part) => (
+        <PartCard part={part} />
+      ))}
+    </section>
+  );
 }
