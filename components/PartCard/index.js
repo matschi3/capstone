8e3e4a380815c6f6c97383a8e57c8c0ae8afcba2
Nils Fischer <nils@designatic.de>
2023-06-23 16:01:11 +0200

    set import right, changed article to styled pondant

--- components/PartCard/index.js
+++ components/PartCard/index.js
@@ -1,13 +1,13 @@
 import Image from "next/image";
-import { PartCardContainer } from "./PartCard.styled";
+import { PartCardFlexContainer } from "./PartCard.styled";
 
 export default function PartCard({ part }) {
   return (
-    <article>
+    <PartCardFlexContainer direction={"row"}>
       <Image src={part.imgUrl} alt={part.name} width={100} height={100} />
       <p>Einkaufspreis: {part.purchasingPrice}</p>
       <p>Name: {part.name}</p>
       <p>{part.category}</p>
-    </article>
+    </PartCardFlexContainer>
   );
 }
