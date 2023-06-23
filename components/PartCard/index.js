3036eec5c9ff4b488198332086e263dc2dfd5090
Nils Fischer <nils@designatic.de>
2023-06-23 11:54:18 +0200

    setup basic return

--- components/PartCard/index.js
+++ components/PartCard/index.js
@@ -1,3 +1,16 @@
 import { parts } from "@/lib/parts";
 
-export default function PartCard(params) {}
+export default function PartCard(params) {
+  return (
+    <section>
+      {parts.map((part) => (
+        <article key={part._id}>
+          <Image src={part.imgUrl} alt={part.name} width={100} height={100} />
+          <p>Einkaufspreis: {part.purchasingPrice}</p>
+          <p>Name: {part.name}</p>
+          <p>{part.category}</p>
+        </article>
+      ))}
+    </section>
+  );
+}
