db8465ea1bd35bbc0b1c8a5a404c294829e8dc31
Nils Fischer <nils@designatic.de>
2023-06-26 13:32:37 +0200

    add style

--- components/StatusMarker/StatusMarker.styled.js
+++ components/StatusMarker/StatusMarker.styled.js
@@ -1,3 +1,8 @@
 import styled from "styled-components";
 
-export const StatMarker = styled.p``;
+export const StatMarker = styled.p`
+  border: 1px solid black;
+  border-radius: 5px;
+  padding: 0.5%;
+  margin: 0.5%;
+`;
