effbbb6c7e0001138727660fc9d1c3322ddbdb67
Nils Fischer <nils@designatic.de>
2023-06-26 13:29:03 +0200

    add return for isAssembled set true or false

--- components/StatusMarker/index.js
+++ components/StatusMarker/index.js
@@ -1,3 +1,8 @@
 export default function StatusMarker({ part }) {
-  return <>{part.inAssembler === true ? <p>in Verarbeitung</p> : null}</>;
+  return (
+    <>
+      {part.inAssembler === true ? <p>in Verarbeitung</p> : null}
+      {part.isAssembled === true ? <p>verbaut</p> : <p>unverbaut</p>}
+    </>
+  );
 }
