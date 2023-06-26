7e523f98e435d2311617dd80db5dad88eacaee28
Nils Fischer <nils@designatic.de>
2023-06-26 13:38:16 +0200

    import StatusMarker component

--- components/PartCard/index.js
+++ components/PartCard/index.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { PartCardFlexContainer, PartCardCategory } from "./PartCard.styled";
+import StatusMarker from "../StatusMarker";
 
 export default function PartCard({ part }) {
   return (
