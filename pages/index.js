13039d6fd88a356855e48ed6c6f8911e7c629b5f
Nils Fischer <nils@designatic.de>
2023-06-28 15:31:29 +0200

    setup new Header

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,18 @@
 import { parts } from "@/lib/parts";
 import PartsList from "@/components/PartsList";
+import StyledHeader from "@/components/StyledHeader";
+import LinkTo from "@/components/LinkTo";
 
 export default function HomePage() {
-  return <PartsList parts={parts} />;
+  return (
+    <>
+      <StyledHeader title={"TEILE"} color={"var(--color-part)"} />
+      <LinkTo
+        href={"/create-part"}
+        name={"neues TEIL"}
+        color={"var(--color-part)"}
+      />
+      <PartsList parts={parts} />;
+    </>
+  );
 }
