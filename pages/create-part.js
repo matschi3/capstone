20b03b5abec561db2137cf2210c7e3d6be277e9d
Nils Fischer <nils@designatic.de>
2023-06-28 15:55:51 +0200

    setup CreatePartPage

--- pages/create-part.js
+++ pages/create-part.js
@@ -2,3 +2,19 @@ import PartForm from "@/components/PartForm";
 import usePartStore from "@/components/PartStore/UsePartStore";
 import StyledHeader from "@/components/StyledHeader";
 import LinkBack from "@/components/LinkBack";
+
+export default function CreatePartPage() {
+  const { parts, setParts } = usePartStore();
+
+  function createPart(newPart) {
+    setParts(newPart);
+  }
+
+  return (
+    <>
+      <StyledHeader title={"neues TEIL"} color={"var(--color-part)"} />
+      <LinkBack />
+      <PartForm onSubmit={createPart} formName={"create-part"} />
+    </>
+  );
+}
