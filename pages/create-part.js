d9a98c87ef3087a53896cc020113342b753fcf63
Nils Fischer <nils@designatic.de>
2023-06-28 15:55:02 +0200

    init create-part Page with imports

---
+++ pages/create-part.js
@@ -0,0 +1,4 @@
+import PartForm from "@/components/PartForm";
+import usePartStore from "@/components/PartStore/UsePartStore";
+import StyledHeader from "@/components/StyledHeader";
+import LinkBack from "@/components/LinkBack";
