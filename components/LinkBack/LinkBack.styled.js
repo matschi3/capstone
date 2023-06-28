be23d278e6c87e9bcfbd1527d9f586a8aa73b022
Nils Fischer <nils@designatic.de>
2023-06-28 14:56:43 +0200

    add positioning

--- components/LinkBack/LinkBack.styled.js
+++ components/LinkBack/LinkBack.styled.js
@@ -1,4 +1,8 @@
 import styled from "styled-components";
 import { StyledLink } from "../StyledLink/StyledLink.styled";
 
-export const StyledLinkBack = styled(StyledLink)``;
+export const StyledLinkBack = styled(StyledLink)`
+  position: fixed;
+  top: 0.2rem;
+  left: 0.2rem;
+`;
