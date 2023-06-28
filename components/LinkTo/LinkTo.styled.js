41ba237b0ed5d5632c96c91535465f6599865563
Nils Fischer <nils@designatic.de>
2023-06-28 15:05:40 +0200

    add style + positioning

--- components/LinkTo/LinkTo.styled.js
+++ components/LinkTo/LinkTo.styled.js
@@ -1,4 +1,9 @@
 import styled from "styled-components";
 import { StyledLink } from "../StyledLink/StyledLink.styled";
 
-export const StyledLinkTo = styled(StyledLink)``;
+export const StyledLinkTo = styled(StyledLink)`
+  position: fixed;
+  top: 0.2rem;
+  right: 0.2rem;
+  color: ${({ color }) => (color ? `${color}` : `var(--color-black)`)};
+`;
