29e721d5358252f338b22f8f5f49acd26367568e
Nils Fischer <nils@designatic.de>
2023-06-28 14:53:04 +0200

    add style

--- components/StyledLink/StyledLink.styled.js
+++ components/StyledLink/StyledLink.styled.js
@@ -1,4 +1,12 @@
 import styled from "styled-components";
 import Link from "next/link";
 
-export const StyledLink = styled(Link)``;
+export const StyledLink = styled(Link)`
+  text-decoration: none;
+  font-weight: bold;
+  color: var(--color-black);
+  border: 1px solid black;
+  border-radius: 5px;
+  padding: 0.1rem;
+  margin: 0.1rem;
+`;
