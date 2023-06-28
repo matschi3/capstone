5e5589c6c70cc6021d5d3495a9bff26e13c7703d
Nils Fischer <nils@designatic.de>
2023-06-28 15:57:14 +0200

    add router to send user back after Part created

--- pages/create-part.js
+++ pages/create-part.js
@@ -2,12 +2,15 @@ import PartForm from "@/components/PartForm";
 import usePartStore from "@/components/PartStore/UsePartStore";
 import StyledHeader from "@/components/StyledHeader";
 import LinkBack from "@/components/LinkBack";
+import { useRouter } from "next/router";
 
 export default function CreatePartPage() {
   const { parts, setParts } = usePartStore();
+  const router = useRouter();
 
   function createPart(newPart) {
     setParts(newPart);
+    router.push("/");
   }
 
   return (
