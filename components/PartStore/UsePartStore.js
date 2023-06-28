78b57b4a63a8bca284d5046ef5d7494db8bff1ca
Nils Fischer <nils@designatic.de>
2023-06-28 15:50:33 +0200

    setup PartStore for access part/setPart global

--- components/PartStore/UsePartStore.js
+++ components/PartStore/UsePartStore.js
@@ -1,2 +1,12 @@
 import { create } from "zustand";
 import { initialParts } from "@/lib/parts";
+
+const usePartStore = create((set) => ({
+  parts: initialParts,
+  setParts: (newPart) =>
+    set((state) => ({
+      parts: [...state.parts, newPart],
+    })),
+}));
+
+export default usePartStore;
