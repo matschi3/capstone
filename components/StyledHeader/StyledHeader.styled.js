264885800620fb7fa965ab05af346ed3d965e2eb
Nils Fischer <nils@designatic.de>
2023-06-28 15:13:28 +0200

    add positioning + style depending on prop

--- components/StyledHeader/StyledHeader.styled.js
+++ components/StyledHeader/StyledHeader.styled.js
@@ -1,3 +1,14 @@
 import styled from "styled-components";
 
-export const StyledHeadline = styled.h1``;
+export const StyledHeadline = styled.h1`
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  top: 0;
+  margin-block-start: 0;
+  margin-block-end: 0;
+  padding-left: 100vw;
+  padding-right: 100vw;
+  color: ${({ color }) => (color ? `${color}` : `var(--color-black)`)};
+  background-color: var(--color-white);
+`;
