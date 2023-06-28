ae204ac22881d616abe56fd67a2041197d3bbc08
Nils Fischer <nils@designatic.de>
2023-06-28 15:08:31 +0200

    add destructured props

--- components/LinkTo/index.js
+++ components/LinkTo/index.js
@@ -1,6 +1,6 @@
 import { StyledLinkTo } from "./LinkTo.styled";
 
-export default function LinkTo() {
+export default function LinkTo({ href, name, color }) {
   return (
     <StyledLinkTo href={href} color={color}>
       {name}
