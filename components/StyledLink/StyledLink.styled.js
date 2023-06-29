2e7a0429056429ea89e7fb346c2897b7399669b8
Nils Fischer <nils@designatic.de>
2023-06-29 12:26:05 +0200

    style edit / global variables

--- components/StyledLink/StyledLink.styled.js
+++ components/StyledLink/StyledLink.styled.js
@@ -5,7 +5,7 @@ export const StyledLink = styled(Link)`
   text-decoration: none;
   font-weight: bold;
   color: var(--color-black);
-  border: 1px solid black;
+  border: var(--border-sizeButton) solid var(--color-black);
   border-radius: 5px;
   padding: 0.1rem;
   margin: 0.1rem;
