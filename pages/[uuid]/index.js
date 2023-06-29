b1519e614a2a28575796f69533b3c89271928bd2
Nils Fischer <nils@designatic.de>
2023-06-29 12:02:30 +0200

    setup basic return

--- pages/[uuid]/index.js
+++ pages/[uuid]/index.js
@@ -3,4 +3,11 @@ import LinkBack from "../../components/LinkBack/index.js";
 import { PartsListContainer } from "../../components/PartsList/PartsList.styled.js";
 import PartCard from "../../components/PartCard/index.js";
 
-export default function PartDetailPage() {}
+export default function PartDetailPage() {
+  return (
+    <>
+      <StyledHeader title={"TEIL"} color={"var(--color-part)"} />
+      <LinkBack />
+    </>
+  );
+}
