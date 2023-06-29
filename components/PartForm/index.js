02035c1257b2ef209759c50412fb20113339d4f9
Nils Fischer <nils@designatic.de>
2023-06-29 12:12:02 +0200

    import uuidv4

--- components/PartForm/index.js
+++ components/PartForm/index.js
@@ -2,6 +2,7 @@ import { categories } from "../../lib/categories.js";
 import { FormContainer, Label, Input, Select } from "./PartForm.styled.js";
 import { PartCardFlexContainer } from "../PartCard/PartCard.styled.js";
 import { PartsListContainer } from "../PartsList/PartsList.styled.js";
+import { v4 as uuidv4 } from "uuid";
 
 export default function PartForm({ onSubmit, formName, defaultData }) {
   function handleSubmit(event) {
