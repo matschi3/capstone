b7ce84b02d20aaaf562f3af4086eb7683e285c05
Nils Fischer <nils@designatic.de>
2023-06-29 12:04:20 +0200

    import PartStore from zustand

--- pages/[uuid]/index.js
+++ pages/[uuid]/index.js
@@ -2,6 +2,7 @@ import StyledHeader from "../../components/StyledHeader/index.js";
 import LinkBack from "../../components/LinkBack/index.js";
 import { PartsListContainer } from "../../components/PartsList/PartsList.styled.js";
 import PartCard from "../../components/PartCard/index.js";
+import usePartStore from "../../components/PartStore/UsePartStore.js";
 
 export default function PartDetailPage() {
   return (
