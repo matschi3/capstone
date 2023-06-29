e083e73fb9a3286c98fc6f191f858ab1e5edaaad
Nils Fischer <nils@designatic.de>
2023-06-29 12:06:52 +0200

    use PartStore to give specific part depending on given uuid in router.query

--- pages/[uuid]/index.js
+++ pages/[uuid]/index.js
@@ -3,14 +3,21 @@ import LinkBack from "../../components/LinkBack/index.js";
 import { PartsListContainer } from "../../components/PartsList/PartsList.styled.js";
 import PartCard from "../../components/PartCard/index.js";
 import usePartStore from "../../components/PartStore/UsePartStore.js";
+import { useRouter } from "next/router";
 
 export default function PartDetailPage() {
+  const { parts, setParts } = usePartStore();
+
+  const router = useRouter();
+  const { uuid } = router.query;
+  const detailPart = parts.find((part) => part.uuid === uuid);
+
   return (
     <>
       <StyledHeader title={"TEIL"} color={"var(--color-part)"} />
       <LinkBack />
       <PartsListContainer>
-        <PartCard part={part} isDetail />
+        <PartCard part={detailPart} isDetail />
       </PartsListContainer>
     </>
   );
