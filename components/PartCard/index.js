050c5af2322c4c369bf3a49ec10113692332897f
Nils Fischer <nils@designatic.de>
2023-06-30 08:59:09 +0200

    edit prop for justify-content

--- components/PartCard/index.js
+++ components/PartCard/index.js
@@ -15,7 +15,7 @@ export default function PartCard({ part, isDetail }) {
   }
   return (
     <PartCardFlexContainer direction={"column"} border={"blue"}>
-      <PartCardFlexContainer direction={"row"} justify={"space-between"}>
+      <PartCardFlexContainer direction={"row"} justify={"flex-start"}>
         <Link href={!isDetail ? `${part.uuid}` : `/`}>
           <PartCardImage
             src={part.imgUrl}
