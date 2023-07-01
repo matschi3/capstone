ec10dc7e06fe12b2b61befd67c7e585adeb7bb5d
Nils Fischer <nils@designatic.de>
2023-07-01 16:22:43 +0200

    add StyledHeader  into return statement

--- pages/create-item.js
+++ pages/create-item.js
@@ -1,3 +1,10 @@
 import StyledHeader from "../components/StyledHeader/index.js";
+import LinkBack from "../components/LinkBack/index.js";
 
-export default function CreateItemPage() {}
+export default function CreateItemPage() {
+  return (
+    <>
+      <StyledHeader title="neues ITEM" color="var(--color-inAssembler)" />
+    </>
+  );
+}
