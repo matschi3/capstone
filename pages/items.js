a04ebc0ee089c1e62ce6a207299282b155642e67
Nils Fischer <nils@designatic.de>
2023-07-01 19:42:37 +0200

    setup base content into return statement

--- pages/items.js
+++ pages/items.js
@@ -1,1 +1,11 @@
-export default function ItemsPage() {}
+import StyledHeader from "../components/StyledHeader/index.js";
+import LinkBack from "../components/LinkBack/index.js";
+
+export default function ItemsPage() {
+  return (
+    <>
+      <StyledHeader title="ITEMS" color="var(--color-item)" />
+      <LinkBack />
+    </>
+  );
+}
