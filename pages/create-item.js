972048c50f75c56b5747257b4eb3f631d30fa384
Nils Fischer <nils@designatic.de>
2023-07-01 16:31:43 +0200

    add import for PartsListContainer

--- pages/create-item.js
+++ pages/create-item.js
@@ -1,10 +1,12 @@
 import StyledHeader from "../components/StyledHeader/index.js";
 import LinkBack from "../components/LinkBack/index.js";
+import { PartsListContainer } from "../components/PartsList/PartsList.styled.js";
 
 export default function CreateItemPage() {
   return (
     <>
       <StyledHeader title="neues ITEM" color="var(--color-inAssembler)" />
+      <LinkBack />
     </>
   );
 }
