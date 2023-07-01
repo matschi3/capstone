3e60372e7119e788204f55e166f354a4def8a46a
Nils Fischer <nils@designatic.de>
2023-07-01 16:44:44 +0200

    import PartCard

--- pages/create-item.js
+++ pages/create-item.js
@@ -1,6 +1,7 @@
 import StyledHeader from "../components/StyledHeader/index.js";
 import LinkBack from "../components/LinkBack/index.js";
 import { PartsListContainer } from "../components/PartsList/PartsList.styled.js";
+import PartCard from "../components/PartCard/index.js";
 
 export default function CreateItemPage() {
   return (
