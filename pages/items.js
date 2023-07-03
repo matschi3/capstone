5d56f8227e8079a4a20bbb93c83a9d5bee515052
Nils Fischer <nils@designatic.de>
2023-07-03 10:45:24 +0200

    replaced 'initialItems' with items from ItemStore

--- pages/items.js
+++ pages/items.js
@@ -1,14 +1,15 @@
 import StyledHeader from "../components/StyledHeader/index.js";
 import LinkBack from "../components/LinkBack/index.js";
-import { initialItems } from "../lib/items.js";
 import ItemCard from "../components/ItemCard/index.js";
+import useItemStore from "../components/UseStore/UseItemStore.js";
 
 export default function ItemsPage() {
+  const { items, setItems } = useItemStore();
   return (
     <>
       <StyledHeader title="ITEMS" color="var(--color-item)" />
       <LinkBack />
-      {initialItems.map((item) => (
+      {items.map((item) => (
         <ItemCard key={item.uuid} item={item} />
       ))}
     </>
