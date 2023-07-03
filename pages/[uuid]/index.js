64b29214de1d3e37e55f4fc3b44269f073aedfab
Nils Fischer <nils@designatic.de>
2023-07-03 09:21:10 +0200

    fixed import for renamed dir

--- pages/[uuid]/index.js
+++ pages/[uuid]/index.js
@@ -2,7 +2,7 @@ import StyledHeader from "../../components/StyledHeader/index.js";
 import LinkBack from "../../components/LinkBack/index.js";
 import { PartsListContainer } from "../../components/PartsList/PartsList.styled.js";
 import PartCard from "../../components/PartCard/index.js";
-import usePartStore from "../../components/PartStore/UsePartStore.js";
+import usePartStore from "../../components/UseStore/UsePartStore.js";
 import { useRouter } from "next/router";
 
 export default function PartDetailPage() {
