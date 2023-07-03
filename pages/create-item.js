6972638b221645bbdd27cde9ef2be4d532d01116
Nils Fischer <nils@designatic.de>
2023-07-03 10:48:40 +0200

    setup handleCreateItem for setting new Item / editing used parts missing

--- pages/create-item.js
+++ pages/create-item.js
@@ -3,14 +3,49 @@ import LinkBack from "../components/LinkBack/index.js";
 import { PartsListContainer } from "../components/PartsList/PartsList.styled.js";
 import PartCard from "../components/PartCard/index.js";
 import usePartStore from "../components/UseStore/UsePartStore.js";
+import useItemStore from "../components/UseStore/UseItemStore.js";
 import { StyledButton } from "../components/StyledButton/StyledButton.styled.js";
+import { v4 as uuidv4 } from "uuid";
+import { useRouter } from "next/router.js";
 
 export default function CreateItemPage() {
   const { parts, setParts } = usePartStore();
   const inAssemblerParts = parts.filter((part) => part.inAssembler === true);
+  const { items, setItems } = useItemStore();
+  const router = useRouter();
 
   function handleCreateItem() {
-    console.log("success");
+    const allDate = new Date();
+    const day = allDate.getDate();
+    const month = allDate.getMonth() + 1;
+    const year = allDate.getFullYear();
+    const todayDate = day + "." + month + "." + year;
+
+    /*   let partsUuids = []; */
+    /* const partsUuids = inAssemblerParts.map((part) => part.uuid);
+    console.log(partsUuids); */
+
+    const newItem = {
+      uuid: uuidv4(),
+      name: "",
+      dateAssembled: todayDate,
+      parts: inAssemblerParts.map((part) => part.uuid),
+      totalPurchasingPrice: inAssemblerParts.reduce(
+        (sum, part) => sum + part.purchasingPrice,
+        0
+      ),
+      targetPrice: "",
+      soldForPrice: "",
+      currency: "EUR",
+      dateSold: "",
+      imgUrl:
+        "https://res.cloudinary.com/dn4pswuzt/image/upload/v1688228715/etagere_sqk9al.jpg",
+      isSold: false,
+    };
+
+    console.log(newItem);
+    setItems(newItem);
+    router.push("/items");
   }
 
   return (
