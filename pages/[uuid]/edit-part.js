40c56f2fd289352329cf2b7cdfe4fecbf3c448b1
Nils Fischer <nils@designatic.de>
2023-07-04 17:15:37 +0200

    get the correct part-data from partsStore + query

--- pages/[uuid]/edit-part.js
+++ pages/[uuid]/edit-part.js
@@ -5,11 +5,16 @@ import usePartStore from "../../components/UseStore/UsePartStore.js";
 import { useRouter } from "next/router";
 
 export default function EditPartPage() {
+  const { parts, setParts } = usePartStore();
+  // get specific-part data for PartForms defaultData from query
+  const router = useRouter();
+  const { uuid } = router.query;
+  const editPart = parts.find((part) => part.uuid === uuid);
   return (
     <>
       <StyledHeader title="TEIL bearbeiten" color="var(--color-part)" />
       <LinkTo href="/" name="← zurück" posbt="top" poslr="left" />
-      <PartForm formName="edit-part" defaultData={} />
+      <PartForm formName="edit-part" defaultData={editPart} />
     </>
   );
 }
