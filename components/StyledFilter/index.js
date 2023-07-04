eaec3e0eab519851c43a60ab0ebc8ed1fdc63b8a
Nils Fischer <nils@designatic.de>
2023-07-04 11:19:09 +0200

    add comment

--- components/StyledFilter/index.js
+++ components/StyledFilter/index.js
@@ -1,6 +1,7 @@
 import { FilterButtonContainer, FilterButton } from "./StyledFilter.styled";
 
 export default function StyledFilter({ sets }) {
+  // Mapping over the "sets" array and rendering a FilterButton component for each element
   return (
     <FilterButtonContainer>
       {sets.map((set) => (
