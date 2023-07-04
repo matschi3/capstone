eaf194245ffa22cbdbc2837812a84d8a765ad840
Nils Fischer <nils@designatic.de>
2023-07-04 10:22:30 +0200

    init styled.section as FilterContainer for positioning

--- components/StyledFilter/StyledFilter.styled.js
+++ components/StyledFilter/StyledFilter.styled.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 
+export const FilterContainer = styled.section``;
+
 export const FilterButtonContainer = styled.section`
   display: flex;
   white-space: nowrap;
