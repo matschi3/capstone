8b05745a7438c40cfd8679336ce7c9476ec4ad76
Nils Fischer <nils@designatic.de>
2023-07-04 17:10:50 +0200

    import PartStore and useRouter

--- pages/[uuid]/edit-part.js
+++ pages/[uuid]/edit-part.js
@@ -1,13 +1,15 @@
 import StyledHeader from "../../components/StyledHeader/index.js";
 import LinkTo from "../../components/LinkTo/index.js";
 import PartForm from "../../components/PartForm/index.js";
+import usePartStore from "../../components/UseStore/UsePartStore.js";
+import { useRouter } from "next/router";
 
 export default function EditPartPage() {
   return (
     <>
       <StyledHeader title="TEIL bearbeiten" color="var(--color-part)" />
       <LinkTo href="/" name="← zurück" posbt="top" poslr="left" />
-      <PartForm />
+      <PartForm formName="edit-part" defaultData={} />
     </>
   );
 }
