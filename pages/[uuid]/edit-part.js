2b5861cc86fa06c3313720a866fea884936e307f
Nils Fischer <nils@designatic.de>
2023-07-04 16:36:52 +0200

    setup basic return statement

--- pages/[uuid]/edit-part.js
+++ pages/[uuid]/edit-part.js
@@ -1,4 +1,11 @@
 import StyledHeader from "../../components/StyledHeader/index.js";
 import LinkTo from "../../components/LinkTo/index.js";
 
-export default function EditPartPage() {}
+export default function EditPartPage() {
+  return (
+    <>
+      <StyledHeader title="TEIL bearbeiten" color="var(--color-part)" />
+      <LinkTo href="/" name="← zurück" posbt="top" />
+    </>
+  );
+}
