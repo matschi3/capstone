dd3a945186d3cbd57dc7c345646cfb6ed22b0509
Nils Fischer <nils@designatic.de>
2023-07-04 15:56:53 +0200

    optimise conditionally assingment of css property

--- components/StyledButton/StyledButton.styled.js
+++ components/StyledButton/StyledButton.styled.js
@@ -1,13 +1,14 @@
 import styled from "styled-components";
 
 export const StyledButton = styled.button`
-  position: fixed;
-  // if there is a prop posbt or poslr, then use it, otherwise use nothing
+  /* position: fixed; */
+  // if there is a prop 'posbt' or 'poslr', then use it, otherwise use nothing
+  ${(props) => (props.posbt || props.poslr ? "position: fixed" : "")};
   ${(props) => (props.posbt ? `${props.posbt}: 0.2rem` : "")};
   ${(props) => (props.poslr ? `${props.poslr}: 0.2rem` : "")};
   color: ${({ color }) => (color ? `${color}` : `var(--color-black)`)};
   font-weight: bold;
-  font-size: 1em;
+  font-size: ${({ fontsize }) => (fontsize ? `${fontsize}` : "")};
   border: var(--border-sizeButton) solid var(--color-black);
   border-radius: 5px;
   padding: 0.1rem;
