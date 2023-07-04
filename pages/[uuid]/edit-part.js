c0c43edf0c8b0115ae93b1dd96d9d598ae32bd31
Nils Fischer <nils@designatic.de>
2023-07-04 17:05:59 +0200

    insert partForm - it is already ready for edit/default data

--- pages/[uuid]/edit-part.js
+++ pages/[uuid]/edit-part.js
@@ -1,11 +1,13 @@
 import StyledHeader from "../../components/StyledHeader/index.js";
 import LinkTo from "../../components/LinkTo/index.js";
+import PartForm from "../../components/PartForm/index.js";
 
 export default function EditPartPage() {
   return (
     <>
       <StyledHeader title="TEIL bearbeiten" color="var(--color-part)" />
       <LinkTo href="/" name="← zurück" posbt="top" poslr="left" />
+      <PartForm />
     </>
   );
 }
