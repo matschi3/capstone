977a7fa1dac375a000a9a8027d6fb107dd636afa
Nils Fischer <nils@designatic.de>
2023-07-05 11:49:09 +0200

    deleted, because of reUsable LinkTo

--- components/LinkBack/LinkBack.styled.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-import { StyledLink } from "../../components/StyledLink/StyledLink.styled.js";
-
-export const StyledLinkBack = styled(StyledLink)`
-  position: fixed;
-  top: 0.2rem;
-  left: 0.2rem;
-`;
