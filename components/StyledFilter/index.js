63577f410c70bb5b7a10e3924aaa0db443030b05
Nils Fischer <nils@designatic.de>
2023-07-05 13:25:10 +0200

    add prop 'activeFilter + add function to setActive filter REUSABLE also for statusFilter

--- components/StyledFilter/index.js
+++ components/StyledFilter/index.js
@@ -1,11 +1,18 @@
 import { FilterButtonContainer, FilterButton } from "./StyledFilter.styled";
 
-export default function StyledFilter({ sets }) {
+export default function StyledFilter({ sets, activeFilter }) {
   // Mapping over the "sets" array and rendering a FilterButton component for each element with the given "function", "value to set" and "name"
   return (
     <FilterButtonContainer>
       {sets.map((set) => (
-        <FilterButton key={set.name} onClick={() => set.funct(set.value)}>
+        <FilterButton
+          key={set.name}
+          onClick={() => {
+            set.funct(set.value);
+            set.funct2(set.value2);
+          }}
+          isActiveFilter={set.value2 === activeFilter}
+        >
           {set.name}
         </FilterButton>
       ))}
