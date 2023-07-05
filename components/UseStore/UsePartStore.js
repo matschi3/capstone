db060fa61c96773ee9453aac3794a8d394874efb
Nils Fischer <nils@designatic.de>
2023-07-05 16:26:51 +0200

    add comments on 'zustand's BearStore

--- components/UseStore/UsePartStore.js
+++ components/UseStore/UsePartStore.js
@@ -3,16 +3,19 @@ import { initialParts } from "../../lib/parts.js";
 
 const usePartStore = create((set) => ({
   parts: initialParts,
+  // add a new part to the 'parts' array in state
   setParts: (newPart) =>
     set((state) => ({
       parts: [...state.parts, newPart],
     })),
+  // update a parts value to the 'parts' array in state
   updatePartValue: (uuid, key, newValue) =>
     set((state) => ({
       parts: state.parts.map((part) =>
         part.uuid === uuid ? { ...part, [key]: newValue } : part
       ),
     })),
+  // toggle a part value from the 'parts' array in state
   togglePartValue: (uuid, key) =>
     set((state) => ({
       parts: state.parts.map((part) =>
