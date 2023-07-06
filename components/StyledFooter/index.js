a00942ce0760319b2cc3847b08b17fa9393b9955
Nils Fischer <nils@designatic.de>
2023-07-06 10:32:32 +0200

    del StyledNavigation // no need

--- components/StyledFooter/index.js
+++ components/StyledFooter/index.js
@@ -1,14 +1,11 @@
-import { StyledFooterElement, StyledNavigation } from "./StyledFooter.styled";
+import { StyledFooterElement } from "./StyledFooter.styled";
 import LinkTo from "../LinkTo/index.js";
 
 export default function StyledFooter() {
   return (
     <StyledFooterElement>
-      <StyledNavigation>
-        <LinkTo href="/" name="TEILE" color="var(--color-part)" />
-        <LinkTo href="/items" name="ITEMS" color="var(--color-item)" />
-        hello, im the footer
-      </StyledNavigation>
+      <LinkTo href="/" name="TEILE" color="var(--color-part)" />
+      <LinkTo href="/items" name="ITEMS" color="var(--color-item)" />
     </StyledFooterElement>
   );
 }
