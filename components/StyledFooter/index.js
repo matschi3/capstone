ef27eee1e3afa0052b34e8315463897b48a4b51b
Nils Fischer <nils@designatic.de>
2023-07-06 23:15:36 +0200

    fixed import naming + add fontsize to navLinks

--- components/StyledFooter/index.js
+++ components/StyledFooter/index.js
@@ -1,11 +1,16 @@
-import { StyledFooterElement } from "./StyledFooter.styled";
+import { StyledFooterElement } from "./StyledFooter.styled.js";
 import LinkTo from "../LinkTo/index.js";
 
 export default function StyledFooter() {
   return (
     <StyledFooterElement>
-      <LinkTo href="/" name="TEILE" color="var(--color-part)" />
-      <LinkTo href="/items" name="ITEMS" color="var(--color-item)" />
+      <LinkTo href="/" name="TEILE" color="var(--color-part)" fontsize="20px" />
+      <LinkTo
+        href="/items"
+        name="ITEMS"
+        color="var(--color-item)"
+        fontsize="20px"
+      />
     </StyledFooterElement>
   );
 }
