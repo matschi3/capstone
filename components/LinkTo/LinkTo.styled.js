78c2a975c9ffed0243258196ecd16e7b228f5a93
Nils Fischer <nils@designatic.de>
2023-07-06 10:27:56 +0200

    add conditional position for props 'posbt or poslr'

--- components/LinkTo/LinkTo.styled.js
+++ components/LinkTo/LinkTo.styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import { StyledLink } from "../../components/StyledLink/StyledLink.styled.js";
 
 export const StyledLinkTo = styled(StyledLink)`
-  position: fixed;
+  ${(props) => (props.posbt || props.poslr ? "position: fixed" : "")};
   ${(props) => props.posbt || "top"}: 0.2rem;
   ${(props) => props.poslr || "right"}: 0.2rem;
   color: ${({ color }) => (color ? `${color}` : `var(--color-black)`)};
