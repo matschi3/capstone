7f2f8adfe96e561f03b024fcba2aef8c49ac8d74
Nils Fischer <nils@designatic.de>
2023-07-06 08:35:00 +0200

    set basic style

--- components/StyledFooter/StyledFooter.styled.js
+++ components/StyledFooter/StyledFooter.styled.js
@@ -1,1 +1,6 @@
-export const StyledFooterElement = styled.footer``;
+export const StyledFooterElement = styled.footer`
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 0;
+`;
