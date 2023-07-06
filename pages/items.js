2d0fa1fad25cbdf14e4808848249abc36ddad86d
Nils Fischer <nils@designatic.de>
2023-07-06 23:05:13 +0200

    add StyledFooter + top prop for LinkTo

--- pages/items.js
+++ pages/items.js
@@ -2,6 +2,7 @@ import StyledHeader from "../components/StyledHeader/index.js";
 import LinkTo from "../components/LinkTo/index.js";
 import ItemCard from "../components/ItemCard/index.js";
 import useItemStore from "../components/UseStore/UseItemStore.js";
+import StyledFooter from "../components/StyledFooter/index.js";
 
 export default function ItemsPage() {
   const { items, setItems } = useItemStore();
@@ -13,10 +14,12 @@ export default function ItemsPage() {
         name="← zurück"
         color="var(--color-inAssembler)"
         poslr="left"
+        posbt="top"
       />
       {items.map((item) => (
         <ItemCard key={item.uuid} item={item} />
       ))}
+      <StyledFooter />
     </>
   );
 }
