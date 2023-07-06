26ac54c43947754b2b436ecbf8d01d465a871824
Nils Fischer <nils@designatic.de>
2023-07-06 09:14:51 +0200

    import LinkTo

--- components/StyledFooter/index.js
+++ components/StyledFooter/index.js
@@ -1,3 +1,6 @@
+import { StyledFooterElement } from "./StyledFooter.styled";
+import LinkTo from "../LinkTo/index.js";
+
 export default function StyledFooter() {
-  return <footer>hello, im the footer</footer>;
+  return <StyledFooterElement>hello, im the footer</StyledFooterElement>;
 }
