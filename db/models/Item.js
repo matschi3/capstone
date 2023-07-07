1f0d9639d369e61da20cb9b3790285365e128b66
Nils Fischer <nils@designatic.de>
2023-07-07 10:58:20 +0200

    setup itemSchema

--- db/models/Item.js
+++ db/models/Item.js
@@ -2,4 +2,16 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
-const itemSchema = new Schema({});
+const itemSchema = new Schema({
+  uuid: { type: String, required: true },
+  name: { type: String, required: false },
+  dateAssembled: { type: String, required: true },
+  parts: { type: [Schema.Types.ObjectId], ref: "Part" },
+  totalPurchasingPrice: { type: Number, required: true },
+  targetPrice: { type: Number, required: false },
+  soldForPrice: { type: Number, required: false },
+  currency: { type: String, required: true },
+  dateSold: { type: String, required: false },
+  imgUrl: { type: String, required: true },
+  isSold: { type: Boolean, required: true },
+});
