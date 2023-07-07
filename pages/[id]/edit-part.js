15b6db6469a2f97554e498b6efc109e23178f144
Nils Fischer <nils@designatic.de>
2023-07-07 20:15:24 +0200

    setup useSWR + isLoading + error handling

--- pages/[id]/edit-part.js
+++ pages/[id]/edit-part.js
@@ -3,17 +3,32 @@ import LinkTo from "../../components/LinkTo/index.js";
 import PartForm from "../../components/PartForm/index.js";
 import usePartStore from "../../components/UseStore/UsePartStore.js";
 import { useRouter } from "next/router";
+import useSWR from "swr";
 
 export default function EditPartPage() {
-  const { parts, setParts } = usePartStore();
+  /*  const { parts, setParts } = usePartStore(); */
   // get specific-part data for PartForms defaultData from query
   const router = useRouter();
-  const { uuid } = router.query;
-  const partToEdit = parts.find((part) => part.uuid === uuid);
+  const { id } = router.query;
+  const { data, isLoading, error } = useSWR(`/api/parts/${id}`);
+
+  if (isLoading) {
+    return <h1>Lädt Teil...</h1>;
+  }
+  if (!data) {
+    return <h1>kein Teil zum bearbeiten gefunden.</h1>;
+  }
+  if (error) {
+    return <h1>error! fehlerhafte Daten.</h1>;
+  }
+
+  const partToEdit = data;
+  /*   const partToEdit = parts.find((part) => part.uuid === uuid); */
 
   async function editPart(newPart) {
+    console.log(newPart);
     // update edited values in state
-    usePartStore.getState().updatePartValue(uuid, "name", newPart.name);
+    /* usePartStore.getState().updatePartValue(uuid, "name", newPart.name);
     usePartStore.getState().updatePartValue(uuid, "category", newPart.category);
     usePartStore
       .getState()
@@ -21,13 +36,13 @@ export default function EditPartPage() {
     await usePartStore
       .getState()
       .updatePartValue(uuid, "partOrigin", newPart.partOrigin);
-    router.push(`/${uuid}`);
+    router.push(`/${uuid}`); */
   }
 
   return (
     <>
       <StyledHeader title="TEIL bearbeiten" color="var(--color-part)" />
-      <LinkTo href={`/${uuid}`} name="← zurück" posbt="top" poslr="left" />
+      <LinkTo href={`/${id}`} name="← zurück" posbt="top" poslr="left" />
       <PartForm
         onSubmit={editPart}
         formName="edit-part"
