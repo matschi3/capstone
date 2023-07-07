b674b854f801032eda84f16643d364eb285453b7
Nils Fischer <nils@designatic.de>
2023-07-07 15:27:49 +0200

    setup GET method

--- pages/api/items/[id].js
+++ pages/api/items/[id].js
@@ -1,4 +1,15 @@
 import dbConnect from "../../../db/connect.js";
 import Item from "../../../db/models/Item.js";
 
-export default async function handler(request, response) {}
+export default async function handler(request, response) {
+  await dbConnect();
+  const { id } = request.query;
+  // get id-specific item from the items db
+  if (request.method === "GET") {
+    const item = await Item.findById(id);
+    if (!item) {
+      return response.status(404).json({ status: "Item nicht gefunden." });
+    }
+    response.status(200).json(item);
+  }
+}
