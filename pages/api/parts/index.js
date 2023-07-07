ee775365ca817df107d5f3a42668ebadac0d3b6e
Nils Fischer <nils@designatic.de>
2023-07-07 11:55:08 +0200

    add try/catch

--- pages/api/parts/index.js
+++ pages/api/parts/index.js
@@ -3,16 +3,19 @@ import Part from "../../../db/models/Part.js";
 
 export default async function handler(request, response) {
   await dbConnect();
-
+  // get all parts from the parts db
   if (request.method === "GET") {
     const parts = await Part.find({});
     return response.status(200).json(parts);
   }
-
+  // create a new part in parts db
   if (request.method === "POST") {
-    const newPart = request.body;
-    await Part.create(newPart);
-    return response.status(201).json({ status: "Part created." });
+    try {
+      const newPart = request.body;
+      await Part.create(newPart);
+      return response.status(201).json({ status: "Part created." });
+    } catch (error) {
+      response.status(400).json({ error: error.message });
+    }
   }
-  response.status(400).json({ error: error.message });
 }
