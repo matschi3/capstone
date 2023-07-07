c83b0d15a77e2b0c877757822061dd465cf7f255
Nils Fischer <nils@designatic.de>
2023-07-07 15:04:23 +0200

    setup GET method

--- pages/api/parts/[id].js
+++ pages/api/parts/[id].js
@@ -5,6 +5,11 @@ export default async function handler(request, response) {
   await dbConnect();
 
   if (request.method === "GET") {
+    const part = await Part.findById(id);
+    if (!part) {
+      return response.status(404).json({ status: "Teil nicht gefunden." });
+    }
+    response.status(200).json(part);
   }
 
   if (request.method === "PUT") {
