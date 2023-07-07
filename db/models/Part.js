924a8d99ede05c7319a99dfe8a42134ce2e9a120
Nils Fischer <nils@designatic.de>
2023-07-07 10:48:22 +0200

    setup partSchema

--- db/models/Part.js
+++ db/models/Part.js
@@ -2,4 +2,18 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
-const partSchema = new Schema({});
+const partSchema = new Schema({
+  uuid: { type: String, required: true },
+  name: { type: String, required: true },
+  dateBuy: { type: String, required: true },
+  dateAssembled: { type: String, required: false },
+  dateSold: { type: String, required: false },
+  category: { type: String, required: true },
+  currency: { type: String, required: true },
+  purchasingPrice: { type: Number, required: true },
+  imgUrl: { type: String, required: true },
+  partOrigin: { type: String, required: false },
+  inAssembler: { type: Boolean, required: true },
+  isAssembled: { type: Boolean, required: true },
+  isSold: { type: Boolean, required: true },
+});
