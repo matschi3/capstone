4648fe8774ec4c61db1f7855b0b8ecea5d41aad6
Nils Fischer <nils@designatic.de>
2023-07-07 11:40:09 +0200

    setup GET method

--- pages/api/parts/index.js
+++ pages/api/parts/index.js
@@ -1,4 +1,12 @@
 import dbConnect from "../../../db/connect.js";
 import Part from "../../../db/models/Part.js";
 
-export default async function handler(request, response) {}
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const parts = await Part.find({});
+    return response.status(200).json(parts);
+  }
+  response.status(400).json({ error: error.message });
+}
