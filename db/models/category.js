cf3e9b67b4374ca1f2ab4db40a0445177db1cfa6
Nils Fischer <nils@designatic.de>
2023-07-07 11:15:01 +0200

    export default mongoose.model for Category

--- db/models/category.js
+++ db/models/category.js
@@ -6,3 +6,8 @@ const categorySchema = new Schema({
   name: { type: String, required: true },
   text: { type: String, required: false },
 });
+
+const Category =
+  mongoose.models.Category || mongoose.model("Category", categorySchema);
+
+export default Category;
