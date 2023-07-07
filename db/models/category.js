9153f8a1b6fddf0de2063be7547d8bb405bb041e
Nils Fischer <nils@designatic.de>
2023-07-07 11:14:04 +0200

    setup categorySchema

--- db/models/category.js
+++ db/models/category.js
@@ -2,4 +2,7 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
-const categorySchema = new Schema({});
+const categorySchema = new Schema({
+  name: { type: String, required: true },
+  text: { type: String, required: false },
+});
