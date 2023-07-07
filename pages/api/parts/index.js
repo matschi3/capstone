cdee90615bf5c74124c6cd2fb650249ce76dfd8c
Nils Fischer <nils@designatic.de>
2023-07-07 11:51:16 +0200

    setup POST method

--- pages/api/parts/index.js
+++ pages/api/parts/index.js
@@ -8,5 +8,11 @@ export default async function handler(request, response) {
     const parts = await Part.find({});
     return response.status(200).json(parts);
   }
+
+  if (request.method === "POST") {
+    const newPart = request.body;
+    await Part.create(newPart);
+    return response.status(201).json({ status: "Part created." });
+  }
   response.status(400).json({ error: error.message });
 }
