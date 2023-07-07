a58079c2f58913bf96f5271e0b897fa9d6a7df42
Nils Fischer <nils@designatic.de>
2023-07-07 15:22:29 +0200

    setup GET method

--- pages/api/items/index.js
+++ pages/api/items/index.js
@@ -1,4 +1,11 @@
 import dbConnect from "../../../db/connect.js";
 import Item from "../../../db/models/Item.js";
 
-export default async function handler(request, response) {}
+export default async function handler(request, response) {
+  await dbConnect();
+  // get all items from the items db + populate parts
+  if (request.method === "GET") {
+    const items = await Item.find({}).populate("parts");
+    return response.status(200).json(items);
+  }
+}
