a0068f75d7f0a03444e70e5ab1c078486eb2bd47
Nils Fischer <nils@designatic.de>
2023-07-07 15:00:57 +0200

    add needed request-methods for a single part

--- pages/api/parts/[id].js
+++ pages/api/parts/[id].js
@@ -1,4 +1,15 @@
 import dbConnect from "../../../db/connect.js";
 import Part from "../../../db/models/Part.js";
 
-export default async function handler(request, response) {}
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+  }
+
+  if (request.method === "PUT") {
+  }
+
+  if (request.method === "DELETE") {
+  }
+}
