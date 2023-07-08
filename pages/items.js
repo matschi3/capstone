b59050bcba3fa4a0fe666819d60214a9ad225d10
Nils Fischer <nils@designatic.de>
2023-07-08 12:23:37 +0200

    del tryarounds

--- pages/items.js
+++ pages/items.js
@@ -16,36 +16,6 @@ export default function ItemsPage() {
     return <h1>error! fehlerhafte Daten.</h1>;
   }
   console.log(items);
-  /* const {
-    data: itemsData,
-    isLoading: itemsIsLoading,
-    error: itemsError,
-  } = useSWR("/api/items");
-  const {
-    dtaa: partsData,
-    isLoading: partsIsLoading,
-    error: partsError,
-  } = useSWR("/api/parts");
-
-  if (itemsIsLoading) {
-    return <h1>lädt Items...</h1>;
-  }
-  if (!itemsData) {
-    return <h1>keine Items gefunden.</h1>;
-  }
-  if (itemsError) {
-    return <h1>error! fehlerhafte Daten.</h1>;
-  }
-
-  if (partsIsLoading) {
-    return <h1>lädt Teile...</h1>;
-  }
-  if (!partsData) {
-    return <h1>keine Teile gefunden.</h1>;
-  }
-  if (partsError) {
-    return <h1>error! fehlerhafte Daten.</h1>;
-  } */
 
   /*  const { items, setItems } = useItemStore(); */
   return (
