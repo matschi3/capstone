187fa235fc1701bbb76f2a9abd056f0817dcb205
Nils Fischer <nils@designatic.de>
2023-07-09 11:56:53 +0200

    del console.logs

--- components/PartForm/index.js
+++ components/PartForm/index.js
@@ -52,7 +52,7 @@ export default function PartForm({ onSubmit, formName, defaultData }) {
 
     onSubmit(newPart);
   }
-  console.log(defaultData);
+
   return (
     <PartsListContainer>
       <PartCardFlexContainer border="var(--color-part)">
@@ -65,7 +65,6 @@ export default function PartForm({ onSubmit, formName, defaultData }) {
             defaultValue={defaultData?.name}
             required
           />
-          {console.log(defaultData?.category[0].name)}
           <Label htmlFor="category">Kategorie</Label>
           <Select
             name="category"
