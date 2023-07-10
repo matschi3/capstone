0690d15276c30c9ff43f2bbda20e2be5fcbab4b9
Nils Fischer <nils@designatic.de>
2023-07-10 17:12:17 +0200

    fix position + fix onSubmit + add form reset()

--- pages/categories.js
+++ pages/categories.js
@@ -28,7 +28,7 @@ export default function CategoriesPage() {
     event.preventDefault();
     const formData = new FormData(event.target);
     const data = Object.fromEntries(formData);
-
+    // create new category object
     const newCategory = {
       name: data.name,
       text: `Standardkategorie für ${data.name}`,
@@ -38,6 +38,7 @@ export default function CategoriesPage() {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(newCategory),
     });
+    event.target.reset();
   }
 
   return (
@@ -46,23 +47,23 @@ export default function CategoriesPage() {
       <PartsListContainer>
         <FormContainer
           aria-labelledby="add new category"
-          onClick={handleAddCategory}
+          onSubmit={handleAddCategory}
         >
           <Label htmlFor="name">neue Kategorie</Label>
           <Input id="name" name="name" type="text" required />
           <button type="submit">hinzufügen</button>
         </FormContainer>
         {categories.map((category) => (
-          <>
-            <PartCardFlexContainer direction="row" justify="space-between">
-              <PartCardCategory key={category._id}>
-                {category.name}
-              </PartCardCategory>
-              <PartCardFlexContainer direction="row" justify="flex-start">
-                {/* place for other category(edit/delete) buttons */}
-              </PartCardFlexContainer>
+          <PartCardFlexContainer
+            key={category._id}
+            direction="row"
+            justify="space-between"
+          >
+            <PartCardCategory>{category.name}</PartCardCategory>
+            <PartCardFlexContainer direction="row" justify="flex-start">
+              {/* place for other category(edit/delete) buttons */}
             </PartCardFlexContainer>
-          </>
+          </PartCardFlexContainer>
         ))}
       </PartsListContainer>
       <StyledFooter />
