ccf58b00eb2b85be2359f98fa473c38cbdbec06f
Nils Fischer <nils@designatic.de>
2023-07-10 15:42:13 +0200

    setup base return statement

--- pages/categories.js
+++ pages/categories.js
@@ -2,4 +2,11 @@ import StyledHeader from "../components/StyledHeader/index.js";
 import StyledFooter from "../components/StyledFooter/index.js";
 import useSWR from "swr";
 
-export default function CategoriesPage() {}
+export default function CategoriesPage() {
+  return (
+    <>
+      <StyledHeader title="KATEGORIEN" color="var(--color-category)" />
+      <StyledFooter />
+    </>
+  );
+}
