5a98d9dd1a42229cc9cd41037d734b5e71314bde
Nils Fischer <nils@designatic.de>
2023-07-10 15:20:39 +0200

    offTopic set refreshInterval to 5sec

--- pages/_app.js
+++ pages/_app.js
@@ -19,7 +19,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
-      <SWRConfig value={{ fetcher, refreshInterval: 1000 }}>
+      <SWRConfig value={{ fetcher, refreshInterval: 5000 }}>
         <Component {...pageProps} />
       </SWRConfig>
     </>
