6b431798a6486389d3cb67ea83854b0eb308417d
Nils Fischer <nils@designatic.de>
2023-07-10 15:44:03 +0200

    setup useSWR for api/categories

--- pages/categories.js
+++ pages/categories.js
@@ -3,6 +3,17 @@ import StyledFooter from "../components/StyledFooter/index.js";
 import useSWR from "swr";
 
 export default function CategoriesPage() {
+  const { data: categories, isLoading, error } = useSWR("/api/categories");
+  if (isLoading) {
+    return <h1>lädt Kategorien...</h1>;
+  }
+  if (!categories) {
+    return <h1>keine Kategorien gefunden.</h1>;
+  }
+  if (error) {
+    return <h1>error! fehlerhafte Daten.</h1>;
+  }
+
   return (
     <>
       <StyledHeader title="KATEGORIEN" color="var(--color-category)" />
