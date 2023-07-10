974b385c8917f054dac171548725f1cdd41358ae
Nils Fischer <nils@designatic.de>
2023-07-10 15:35:11 +0200

    setup base imports

---
+++ pages/categories.js
@@ -0,0 +1,5 @@
+import StyledHeader from "../components/StyledHeader/index.js";
+import StyledFooter from "../components/StyledFooter/index.js";
+import useSWR from "swr";
+
+export default function CategoriesPage() {}
