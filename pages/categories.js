a65cc55c901bf2c427811c6f51b7c9167d7af188
Nils Fischer <nils@designatic.de>
2023-07-10 16:07:08 +0200

    .map() thrue 'categories' + display each with position for upcoming buttons

--- pages/categories.js
+++ pages/categories.js
@@ -1,7 +1,11 @@
 import StyledHeader from "../components/StyledHeader/index.js";
 import StyledFooter from "../components/StyledFooter/index.js";
 import useSWR from "swr";
-import { PartCardCategory } from "../components/PartCard/PartCard.styled.js";
+import {
+  PartCardCategory,
+  PartCardFlexContainer,
+} from "../components/PartCard/PartCard.styled.js";
+import { PartsListContainer } from "../components/PartsList/PartsList.styled.js";
 
 export default function CategoriesPage() {
   const { data: categories, isLoading, error } = useSWR("/api/categories");
@@ -14,10 +18,23 @@ export default function CategoriesPage() {
   if (error) {
     return <h1>error! fehlerhafte Daten.</h1>;
   }
-
   return (
     <>
       <StyledHeader title="KATEGORIEN" color="var(--color-category)" />
+      <PartsListContainer>
+        {categories.map((category) => (
+          <>
+            <PartCardFlexContainer direction="row" justify="space-between">
+              <PartCardCategory key={category._id}>
+                {category.name}
+              </PartCardCategory>
+              <PartCardFlexContainer direction="row" justify="flex-start">
+                {/* place for other category(edit/delete) buttons */}
+              </PartCardFlexContainer>
+            </PartCardFlexContainer>
+          </>
+        ))}
+      </PartsListContainer>
       <StyledFooter />
     </>
   );
