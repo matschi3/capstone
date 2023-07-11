802b038881016f59698657ac0560bc5b004eac88
Nils Fischer <nils@designatic.de>
2023-07-11 17:12:13 +0200

    add check for data

--- components/PartCard/index.js
+++ components/PartCard/index.js
@@ -38,7 +38,6 @@ export default function PartCard({ part, isDetail, isMini }) {
     });
     router.push("/");
   }
-
   return (
     <>
       {isMini ? (
@@ -50,7 +49,7 @@ export default function PartCard({ part, isDetail, isMini }) {
           <PartCardFlexContainer width="15%"></PartCardFlexContainer>
           <PartCardImage
             src={part.imgUrl}
-            alt={part.category[0].name}
+            alt={part.category[0]?.name}
             width={100}
             height={100}
           />
@@ -59,7 +58,7 @@ export default function PartCard({ part, isDetail, isMini }) {
               EK: {part.purchasingPrice} {part.currency}
             </PartCardText>
             <PartCardFlexContainer direction="row" justify="flex-start">
-              <PartCardCategory>{part.category[0].name}</PartCardCategory>
+              <PartCardCategory>{part.category[0]?.name}</PartCardCategory>
             </PartCardFlexContainer>
           </PartCardFlexContainer>
         </PartCardFlexContainer>
@@ -69,7 +68,7 @@ export default function PartCard({ part, isDetail, isMini }) {
             <Link href={!isDetail ? `${part._id}` : `/`}>
               <PartCardImage
                 src={part.imgUrl}
-                alt={part.category[0].name}
+                alt={part.category[0]?.name}
                 width={100}
                 height={100}
               />
@@ -118,7 +117,7 @@ export default function PartCard({ part, isDetail, isMini }) {
             </PartCardFlexContainer>
           </PartCardFlexContainer>
           <PartCardFlexContainer direction="row" justify="flex-start">
-            <PartCardCategory>{part.category[0].name}</PartCardCategory>
+            <PartCardCategory>{part.category[0]?.name}</PartCardCategory>
             <StatusMarker part={part} />
           </PartCardFlexContainer>
         </PartCardFlexContainer>
