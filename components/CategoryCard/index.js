2c4438f221fbec9f99d5584aab9640eb41dd97c6
Nils Fischer <nils@designatic.de>
2023-07-11 12:57:10 +0200

    add useState + CategoryForm for each cards edit

--- components/CategoryCard/index.js
+++ components/CategoryCard/index.js
@@ -3,8 +3,12 @@ import {
   PartCardFlexContainer,
 } from "../PartCard/PartCard.styled.js";
 import { StyledButton } from "../StyledButton/StyledButton.styled.js";
+import { useState } from "react";
+import CategoryForm from "../CategoryForm/index.js";
 
 export default function CategoryCard({ category }) {
+  const [isEdit, setIsEdit] = useState(false);
+
   async function handleEditCategory() {
     console.log("edit category");
   }
@@ -14,21 +18,28 @@ export default function CategoryCard({ category }) {
   }
 
   return (
-    <PartCardFlexContainer
-      key={category._id}
-      direction="row"
-      justify="space-between"
-    >
-      <PartCardCategory>{category.name}</PartCardCategory>
-      <PartCardFlexContainer direction="row" justify="flex-start">
-        <StyledButton onClick={handleEditCategory}>bearbeiten</StyledButton>
-        <StyledButton
-          onClick={handleDeleteCategory}
-          borderColor="var(--color-red)"
-        >
-          löschen
-        </StyledButton>
+    <>
+      <PartCardFlexContainer
+        key={category._id}
+        direction="row"
+        justify="space-between"
+      >
+        <PartCardCategory>{category.name}</PartCardCategory>
+        <PartCardFlexContainer direction="row" justify="flex-start">
+          <StyledButton onClick={handleEditCategory}>bearbeiten</StyledButton>
+          <StyledButton
+            onClick={handleDeleteCategory}
+            borderColor="var(--color-red)"
+          >
+            löschen
+          </StyledButton>
+        </PartCardFlexContainer>
       </PartCardFlexContainer>
-    </PartCardFlexContainer>
+      <CategoryForm
+        onSubmit={handleEditCategory}
+        formName="edit-category"
+        defaultData={category}
+      />
+    </>
   );
 }
