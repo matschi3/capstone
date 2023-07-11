1e8d9a33e554ae6c7db1521cc039373450395954
Nils Fischer <nils@designatic.de>
2023-07-11 10:25:37 +0200

    made it reusable

--- components/CategoryForm/index.js
+++ components/CategoryForm/index.js
@@ -1,14 +1,31 @@
 import { FormContainer, Input, Label } from "../PartForm/PartForm.styled.js";
 
 export default function CategoryForm({ onSubmit, formName, defaultData }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    // create new category object
+    const newCategory = {
+      name: data.name,
+      text: `Kategorie für ${data.name}`,
+    };
+    onSubmit(newCategory);
+  }
+
   return (
-    <FormContainer
-      aria-labelledby="add new category"
-      onSubmit={handleAddCategory}
-    >
+    <FormContainer aria-labelledby={formName} onSubmit={handleSubmit}>
       <Label htmlFor="name">neue Kategorie</Label>
-      <Input id="name" name="name" type="text" required />
-      <button type="submit">hinzufügen</button>
+      <Input
+        id="name"
+        name="name"
+        type="text"
+        defaultValue={defaultData?.name}
+        required
+      />
+      <button type="submit">
+        {defaultData ? "bearbeiten ✓" : "hinzufügen"}
+      </button>
     </FormContainer>
   );
 }
