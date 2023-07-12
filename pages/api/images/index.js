bd11e787b502ab42b1afec4c298d3ac721966ee6
Nils Fischer <nils@designatic.de>
2023-07-12 12:17:01 +0200

    add handler for api route

--- pages/api/images/index.js
+++ pages/api/images/index.js
@@ -14,3 +14,20 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_SECRET,
 });
+
+export default async function handler(request, response) {
+  if (request.method === "GET") {
+    try {
+      // we use cloudinary search API to retrieve images
+      const result = await cloudinary.v2.search
+        // see documentation to adjust the query at https://cloudinary.com/documentation/search_api#examples
+        .with_field("tags")
+        .max_results(10)
+        .execute();
+      // finally we deliver the response with the result as JSON
+      response.status(200).json(result);
+    } catch (error) {
+      response.status(500).json({ message: error.message });
+    }
+  }
+}
