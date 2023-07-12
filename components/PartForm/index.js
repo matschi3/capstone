415135196c2211ed658d8c94b196600d6d7db933
Nils Fischer <nils@designatic.de>
2023-07-12 15:55:58 +0200

    added ImgUpload form into newPartForm bc this is the place and form inside form wasnt working

--- components/PartForm/index.js
+++ components/PartForm/index.js
@@ -4,24 +4,55 @@ import { PartCardFlexContainer } from "../PartCard/PartCard.styled.js";
 import { PartsListContainer } from "../PartsList/PartsList.styled.js";
 import { v4 as uuidv4 } from "uuid";
 import useSWR from "swr";
-import ImageUploadForm from "../ImageUploadForm/index.js";
+import React, { useState } from "react";
 
 export default function PartForm({ onSubmit, formName, defaultData }) {
-  const { data: categories, isLoading, error } = useSWR("/api/categories");
+  // for image Upload; define some states to give some feedback to the user what happened to our upload
+  const { mutate } = useSWR("/api/images/");
+  const [uploadStatus, setUploadStatus] = useState("");
+  const [error, setError] = useState(undefined);
+
+  // categories fetching for select-dropdown
+  const {
+    data: categories,
+    isLoading,
+    error: categoriesError,
+  } = useSWR("/api/categories");
   if (isLoading) {
     return <h1>lädt Kategorien...</h1>;
   }
   if (!categories) {
     return <h1>keine Kategorien gefunden.</h1>;
   }
-  if (error) {
+  if (categoriesError) {
     return <h1>error! fehlerhafte Daten.</h1>;
   }
 
-  function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
+    setUploadStatus("Uploading...");
     const formData = new FormData(event.target);
     const data = Object.fromEntries(formData);
+    console.log(data);
+    // upload image and get url ?!?
+    try {
+      const response = await fetch("/api/upload", {
+        method: "POST",
+        body: formData.image,
+      });
+      // once the file is uploaded (= the promise in our api upload is resolved)
+      if (response.status === 201) {
+        // we call mutate to refresh our image data
+        const { uploadedImgUrl } = await response.json();
+        mutate();
+        // and set a successful state
+        setUploadStatus("Upload complete!");
+      }
+    } catch (error) {
+      // in case of error, we set the state accordingly
+      setError(error);
+    }
+    console.log(uploadedImgUrl);
 
     // get todays date in Format 'dd.mm.yyyy' IF there is no defaultData (edit-part)
     let todayDate;
@@ -56,7 +87,6 @@ export default function PartForm({ onSubmit, formName, defaultData }) {
 
   return (
     <PartsListContainer>
-      <ImageUploadForm />
       <PartCardFlexContainer border="var(--color-part)">
         <FormContainer aria-labelledby={formName} onSubmit={handleSubmit}>
           <Label htmlFor="name">Name</Label>
@@ -71,12 +101,17 @@ export default function PartForm({ onSubmit, formName, defaultData }) {
           <Select
             name="category"
             id="category"
-            defaultValue={defaultData?.category[0]._id}
+            // error handling for: parts category got deleted and is not available anymore for listing or editing
+            defaultValue={
+              defaultData !== undefined
+                ? defaultData.category[0]?._id
+                : "keine Kategorie"
+            }
             required
           >
             <option value="">hier auswählen</option>
             {categories.map((category) => (
-              <option key={category.name} value={category._id}>
+              <option key={category._id} value={category._id}>
                 {category.name}
               </option>
             ))}
@@ -97,7 +132,8 @@ export default function PartForm({ onSubmit, formName, defaultData }) {
             type="text"
             defaultValue={defaultData?.partOrigin}
           />
-
+          <Label htmlFor="image">Foto</Label>
+          <Input type="file" name="image" id="image" />
           <button type="submit">
             {defaultData ? "bearbeitung bestätigen" : "Teil hinzufügen"}
           </button>
