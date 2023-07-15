a2ff58e0520b3b8081ee159bb5a5fd1f6be46244
Nils Fischer <nils@designatic.de>
2023-07-15 12:03:51 +0200

    add isActive as prop

--- components/Popup/index.js
+++ components/Popup/index.js
@@ -2,12 +2,12 @@ import { StyledPopup } from "./Popup.styled.js";
 import { PartCardFlexContainer } from "../PartCard/PartCard.styled.js";
 import { StyledButton } from "../StyledButton/StyledButton.styled";
 
-export default function Popup({ id }) {
+export default function Popup({ id, isActive }) {
   return isActive ? (
     <StyledPopup>
       <PartCardFlexContainer align="center">input</PartCardFlexContainer>
       <PartCardFlexContainer direction="row" justify="space-around">
-        <StyledButton onClick={onClosePopup}>abbrechen</StyledButton>
+        <StyledButton onClick={null}>abbrechen</StyledButton>
         <StyledButton>bestätigen</StyledButton>
       </PartCardFlexContainer>
     </StyledPopup>
