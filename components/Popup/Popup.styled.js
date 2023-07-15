a7ce1f0ad2b718c1284eb10d8bc3f41ed2954ab0
Nils Fischer <nils@designatic.de>
2023-07-15 11:06:07 +0200

    add base style

--- components/Popup/Popup.styled.js
+++ components/Popup/Popup.styled.js
@@ -1,3 +1,16 @@
 import styled from "styled-components";
 
-export const StyledPopup = styled.section``;
+export const StyledPopup = styled.section`
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  top: 30vh;
+  bottom: 30vh;
+  z-index: 1;
+  width: 80vw;
+  height: 40vh;
+  background-color: grey;
+  opacity: 0.75;
+  border-radius: 25px;
+  display: flex;
+`;
