197bf99521fa626b0f4fae4972f3b3eda3aaeb21
Nils Fischer <nils@designatic.de>
2023-07-15 12:02:51 +0200

    del useState + setter-functions

--- components/Popup/index.js
+++ components/Popup/index.js
@@ -1,20 +1,13 @@
-import { useState } from "react";
 import { StyledPopup } from "./Popup.styled.js";
 import { PartCardFlexContainer } from "../PartCard/PartCard.styled.js";
 import { StyledButton } from "../StyledButton/StyledButton.styled";
 
-export default function Popup() {
-  const [isActive, setIsActive] = useState(false);
-
-  function toggleIsActive() {
-    setIsActive(!isActive);
-  }
-
+export default function Popup({ id }) {
   return isActive ? (
     <StyledPopup>
       <PartCardFlexContainer align="center">input</PartCardFlexContainer>
       <PartCardFlexContainer direction="row" justify="space-around">
-        <StyledButton onClick={toggleIsActive}>abbrechen</StyledButton>
+        <StyledButton onClick={onClosePopup}>abbrechen</StyledButton>
         <StyledButton>bestätigen</StyledButton>
       </PartCardFlexContainer>
     </StyledPopup>
