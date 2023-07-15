b8d2ef9e8692bdad093a26c4753412076d978528
Nils Fischer <nils@designatic.de>
2023-07-15 10:46:02 +0200

    import StyledPopup

--- components/ItemCard/index.js
+++ components/ItemCard/index.js
@@ -12,30 +12,33 @@ import Popup from "../Popup/index.js";
 export default function ItemCard({ item }) {
   const [targetPricePopupActive, setTargetPricePopupActive] = useState(false);
   return (
-    <PartsListContainer borderColor="var(--color-item)">
-      <PartCardFlexContainer direction="row" justify="flex-start">
-        <PartCardImage
-          src={item.imgUrl}
-          alt={item.name}
-          width={100}
-          height={100}
-        />
-        <PartCardFlexContainer direction="column" justify="flex-start">
-          <PartCardText>{item.dateAssembled}</PartCardText>
-          <PartCardText>
-            {item.totalPurchasingPrice} {item.currency}
-          </PartCardText>
+    <>
+      <PartsListContainer borderColor="var(--color-item)">
+        <PartCardFlexContainer direction="row" justify="flex-start">
+          <PartCardImage
+            src={item.imgUrl}
+            alt={item.name}
+            width={100}
+            height={100}
+          />
+          <PartCardFlexContainer direction="column" justify="flex-start">
+            <PartCardText>{item.dateAssembled}</PartCardText>
+            <PartCardText>
+              {item.totalPurchasingPrice} {item.currency}
+            </PartCardText>
+          </PartCardFlexContainer>
+          <PartCardFlexContainer direction="column" justify="flex-start">
+            <StyledButton onClick={console.log("setVK")}>
+              VK einstellen
+            </StyledButton>
+          </PartCardFlexContainer>
         </PartCardFlexContainer>
-        <PartCardFlexContainer direction="column" justify="flex-start">
-          <StyledButton onClick={console.log("setVK")}>
-            VK einstellen
-          </StyledButton>
-        </PartCardFlexContainer>
-      </PartCardFlexContainer>
-      {/* render miniPartCard for each part in populated item.parts (parts of item */}
-      {item.parts.map((part) => (
-        <PartCard key={part._id} part={part} isMini />
-      ))}
-    </PartsListContainer>
+        {/* render miniPartCard for each part in populated item.parts (parts of item */}
+        {item.parts.map((part) => (
+          <PartCard key={part._id} part={part} isMini />
+        ))}
+      </PartsListContainer>
+      <Popup />
+    </>
   );
 }
