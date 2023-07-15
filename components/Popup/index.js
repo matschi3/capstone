b93f539463a7f6655d701dc338013028b619f47c
Nils Fischer <nils@designatic.de>
2023-07-15 12:27:47 +0200

    renam 'abbrechen' button

--- components/Popup/index.js
+++ components/Popup/index.js
@@ -2,12 +2,12 @@ import { StyledPopup } from "./Popup.styled.js";
 import { PartCardFlexContainer } from "../PartCard/PartCard.styled.js";
 import { StyledButton } from "../StyledButton/StyledButton.styled";
 
-export default function Popup({ id, isActive, onClosePopup }) {
+export default function Popup({ id, isActive, onCancel }) {
   return isActive ? (
     <StyledPopup>
       <PartCardFlexContainer align="center">input</PartCardFlexContainer>
       <PartCardFlexContainer direction="row" justify="space-around">
-        <StyledButton onClick={onClosePopup}>abbrechen</StyledButton>
+        <StyledButton onClick={onCancel}>abbrechen</StyledButton>
         <StyledButton>bestätigen</StyledButton>
       </PartCardFlexContainer>
     </StyledPopup>
