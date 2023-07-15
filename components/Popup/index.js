891578d91f363dedb3a13f5f7dc1d153605d1f3d
Nils Fischer <nils@designatic.de>
2023-07-15 11:06:33 +0200

    import + insert Containers

--- components/Popup/index.js
+++ components/Popup/index.js
@@ -1,7 +1,18 @@
 import { useState } from "react";
-import { StyledPopup } from "./Popup.styled";
+import { StyledPopup } from "./Popup.styled.js";
+import { PartCardFlexContainer } from "../PartCard/PartCard.styled.js";
 
 export default function Popup() {
   const [isActive, setIsActive] = useState(false);
-  return <StyledPopup>lol</StyledPopup>;
+  return (
+    <StyledPopup>
+      <PartCardFlexContainer direction="column" justify="flex-start">
+        <PartCardFlexContainer>input</PartCardFlexContainer>
+        <PartCardFlexContainer>
+          <button>abbrechen</button>
+          <button>bestätigen</button>
+        </PartCardFlexContainer>
+      </PartCardFlexContainer>
+    </StyledPopup>
+  );
 }
