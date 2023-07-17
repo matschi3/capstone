42eaf5fd6ef645961c5e43aad0ed637c508e7607
Nils Fischer <nils@designatic.de>
2023-07-17 16:19:49 +0200

    setup filter/sort buttons based on StyledFilter component

--- pages/items.js
+++ pages/items.js
@@ -24,6 +24,43 @@ export default function ItemsPage() {
   return (
     <>
       <StyledHeader title="ITEMS" color="var(--color-item)" />
+      <FilterContainer>
+        {/* render a filterButton for each object in 'sets' with given 'function', 'value to set' and 'name' */}
+        {/* funct2 + value2 for activeFilter-highlighting */}
+        <StyledFilter
+          filters={[
+            {
+              functionToSet: setSorting,
+              valueToSet: "verbaut(neueste)",
+              buttonName: "verbaut (neueste)",
+              activeFunctionToSet: setActiveSorting,
+              activeValueToSet: "verbaut(neueste)",
+            },
+            {
+              functionToSet: setSorting,
+              valueToSet: "verbaut(älteste)",
+              buttonName: "verbaut (älteste)",
+              activeFunctionToSet: setActiveSorting,
+              activeValueToSet: "verbaut(älteste)",
+            },
+            {
+              functionToSet: setSorting,
+              valueToSet: "verkauft(neueste)",
+              buttonName: "verkauft (neueste)",
+              activeFunctionToSet: setActiveSorting,
+              activeValueToSet: "verkauft(neueste)",
+            },
+            {
+              functionToSet: setSorting,
+              valueToSet: "verkauft(älteste)",
+              buttonName: "verkauft (älteste)",
+              activeFunctionToSet: setActiveSorting,
+              activeValueToSet: "verkauft(älteste)",
+            },
+          ]}
+          activeFilter={activeSorting}
+        />
+      </FilterContainer>
       {items.map((item) => (
         <ItemCard key={item._id} item={item} />
       ))}
