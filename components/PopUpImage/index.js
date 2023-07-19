d010c51c0b4ec2db7c5bcce694ab25da81e5a452
Nils Fischer <nils@designatic.de>
2023-07-19 09:56:28 +0200

    import PartCardFlexContainer, StyledButton, ImageUploadForm

--- components/PopUpImage/index.js
+++ components/PopUpImage/index.js
@@ -1,3 +1,5 @@
 import { StyledPopup } from "../Popup/Popup.styled.js";
+import { PartCardFlexContainer } from "../PartCard/PartCard.styled.js";
+import { StyledButton } from "../StyledButton/StyledButton.styled";
 
-export default function PopUpImage() {}
+export default function PopUpImage({ id, name, isActive }) {}
