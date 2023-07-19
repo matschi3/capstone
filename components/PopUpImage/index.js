bb6152ef77e968bdd37c1cb2957bf840194012bf
Nils Fischer <nils@designatic.de>
2023-07-19 10:23:38 +0200

    swapped imports

--- components/PopUpImage/index.js
+++ components/PopUpImage/index.js
@@ -1,6 +1,35 @@
 import { StyledPopup } from "../Popup/Popup.styled.js";
 import { PartCardFlexContainer } from "../PartCard/PartCard.styled.js";
 import { StyledButton } from "../StyledButton/StyledButton.styled.js";
-import ImageUploadForm from "../ImageUploadForm/index.js";
+import { FormContainer, Label, Input } from "../PartForm/PartForm.styled.js";
 
-export default function PopUpImage({ id, name, isActive }) {}
+export default function PopUpImage({
+  id,
+  name,
+  keyToChange,
+  isActive,
+  setInputValue,
+  onCancel,
+  onConfirm,
+}) {
+  return isActive ? (
+    <StyledPopup>
+      <PartCardFlexContainer align="center">
+        {id}
+        <FormContainer aria-labelledby="file" onSubmit={handleImageUpload}>
+          <Label htmlFor="file">Foto</Label>
+          <Input id="file" name="file" type="file" />
+          <button type="submit">Foto hochladen</button>
+          <p>{uploadStatus}</p>
+          {error && <p>{error.message}</p>}
+        </FormContainer>
+      </PartCardFlexContainer>
+      <PartCardFlexContainer direction="row" justify="space-around">
+        <StyledButton onClick={onCancel}>abbrechen</StyledButton>
+        <StyledButton onClick={() => onConfirm(keyToChange)}>
+          bestätigen
+        </StyledButton>
+      </PartCardFlexContainer>
+    </StyledPopup>
+  ) : null;
+}
