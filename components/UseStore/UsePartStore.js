1b543bc445dec2f704c8cd5b8f5a325261675aad
Nils Fischer <nils@designatic.de>
2023-07-23 10:56:44 +0200

    del unnecessary comment / keep bearStores for app growing, zustand will be back for sure :*

--- components/UseStore/UsePartStore.js
+++ components/UseStore/UsePartStore.js
@@ -3,26 +3,22 @@ import { initialParts } from "../../lib/parts.js";
 
 const usePartStore = create((set) => ({
   parts: initialParts,
-  // add a new part to the 'parts' array in state
   setParts: (newPart) =>
     set((state) => ({
       parts: [...state.parts, newPart],
     })),
-  // update a parts value to the 'parts' array in state
   updatePartValue: (uuid, key, newValue) =>
     set((state) => ({
       parts: state.parts.map((part) =>
         part.uuid === uuid ? { ...part, [key]: newValue } : part
       ),
     })),
-  // toggle a part value from the 'parts' array in state
   togglePartValue: (uuid, key) =>
     set((state) => ({
       parts: state.parts.map((part) =>
         part.uuid === uuid ? { ...part, [key]: !part[key] } : part
       ),
     })),
-  // delete a part from the 'parts' array in state
   deletePart: (uuid) =>
     set((state) => ({
       parts: state.parts.filter((part) => part.uuid !== uuid),
