bd915e9b69f2fe8e9791179d401fb4f0795a09cd
Nils Fischer <nils@designatic.de>
2023-07-24 00:17:08 +0200

    reworked color prop

--- components/StyledButton/StyledButton.styled.js
+++ components/StyledButton/StyledButton.styled.js
@@ -4,12 +4,14 @@ export const StyledButton = styled.button`
   ${(props) => (props.posbt || props.poslr ? "position: fixed" : "")};
   ${(props) => (props.posbt ? `${props.posbt}: 0.2rem` : "")};
   ${(props) => (props.poslr ? `${props.poslr}: 0.2rem` : "")};
-  color: ${({ color }) => (color ? `${color}` : `var(--color-black)`)};
+  ${(props) =>
+    props.color ? `color: ${props.color}` : "color: var(--color-black)"};
   font-weight: bold;
   font-size: ${({ fontSize }) => (fontSize ? `${fontSize}` : "")};
   border: var(--border-sizeButton) solid
     ${(props) =>
       props.borderColor ? `${props.borderColor}` : "var(--color-black)"};
+
   ${(props) =>
     props.backgroundColor && `background-color: ${props.backgroundColor}`};
   border-radius: 5px;
