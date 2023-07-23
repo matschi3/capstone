57152330dcb6a4aa5b1cab443cf687569cebffc5
Nils Fischer <nils@designatic.de>
2023-07-23 21:56:44 +0200

    del bold

--- components/StyledLink/StyledLink.styled.js
+++ components/StyledLink/StyledLink.styled.js
@@ -3,11 +3,9 @@ import Link from "next/link";
 
 export const StyledLink = styled(Link)`
   text-decoration: none;
-  font-weight: bold;
   color: var(--color-black);
   border: var(--border-sizeButton) solid var(--color-black);
   border-radius: 5px;
   padding: 0.1rem;
   margin: 0.1rem;
-  font-weight: bold;
 `;
