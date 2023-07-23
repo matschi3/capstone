f37c79ccd9fbd7d52943e7f7f7330f7ff47cc1b9
Nils Fischer <nils@designatic.de>
2023-07-23 11:02:49 +0200

    del unnecessary comment

--- pages/api/items/index.js
+++ pages/api/items/index.js
@@ -3,7 +3,6 @@ import Item from "../../../db/models/Item.js";
 
 export default async function handler(request, response) {
   await dbConnect();
-  // get all items from the items db + populate parts
   if (request.method === "GET") {
     const items = await Item.find({}).populate({
       path: "parts",
@@ -14,7 +13,6 @@ export default async function handler(request, response) {
     }
     return response.status(200).json(items);
   }
-  // create a new item in items db
   if (request.method === "POST") {
     try {
       const newItem = request.body;
